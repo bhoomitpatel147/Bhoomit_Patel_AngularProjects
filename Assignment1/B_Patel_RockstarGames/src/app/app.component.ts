279c968a2542f223aa012b4970b168891842a312
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-05-17 14:43:01 -0400

    Successfully added content to the HTML

--- Assignment1/B_Patel_RockstarGames/src/app/app.component.ts
+++ Assignment1/B_Patel_RockstarGames/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   ContentItem4: Content;
   ContentItem5: Content;
 
-  ListOfContent: List;
+  ListOfContent: List = new List;
 
 
 
@@ -27,7 +27,7 @@ export class AppComponent {
       title: "Grand Theft Auto: Mission Pack #1 - London 1969",
       body: "This is an expansion pack for the 1998 action-adventure game Grand Theft Auto, developed by Rockstar Canada and published by Rockstar Games.",
       author: "Ray Larabie/Dan Houser",
-      imageLink: "https://en.wikipedia.org/wiki/Grand_Theft_Auto:_London_1969#/media/File:Grand_Theft_Auto_London_1969.jpg",
+      imageLink: "https://upload.wikimedia.org/wikipedia/en/d/de/Grand_Theft_Auto_London_1969.jpg",
       type: "Action-adventure",
       hashtags: ["#grandtheftautolondon1969", "#GTA", "#funRockstar"]
     };
@@ -37,7 +37,7 @@ export class AppComponent {
       title: "Monster Truck Madness 2",
       body: "It is the sequel to Monster Truck Madness for the same platform, and was one of the first racing games to feature an online multiplayer mode. Online play for it was available on the MSN Gaming Zone until early 2006.",
       author: "Mark Randel",
-      imageLink: "https://en.wikipedia.org/wiki/Monster_Truck_Madness_2#/media/File:Monster_Truck_Madness_2_Coverart.png",
+      imageLink: "https://upload.wikimedia.org/wikipedia/en/2/22/Monster_Truck_Madness_2_Coverart.png",
       type: "Racing",
       hashtags: ["#race", "#Multiplayer Race", "#carrace"]
     };
@@ -47,7 +47,7 @@ export class AppComponent {
       title: "Earthworm Jim 3D",
       body: "Earthworm Jim 3D is a platform game, the third in the Earthworm Jim series. It is a sequel to Earthworm Jim and Earthworm Jim 2, and the first game in the series to not be developed by Shiny Entertainment, which had recently instituted a strict \"no sequels\" policy.",
       author: "Paul Munro",
-      imageLink: "https://en.wikipedia.org/wiki/Earthworm_Jim_3D#/media/File:Earthworm_Jim_3D_cover.jpg",
+      imageLink: "https://upload.wikimedia.org/wikipedia/en/7/75/Earthworm_Jim_3D_cover.jpg",
       type: "Platformer",
       hashtags: ["#earthwormjim3d", "#platformer"]
     };
@@ -57,7 +57,7 @@ export class AppComponent {
       title: "Thrasher Presents Skate and Destroy",
       body: "Thrasher Presents Skate and Destroy, also known as Thrasher: Skate and Destroy, is a skateboarding video game developed by Z-Axis and released in 1999 for the Sony PlayStation. The game was later published in Japan by UEP Systems on March 4, 2000 under the name Thrasher SK8.[2] A Game Boy Color version was also developed, but was later cancelled.",
       author: "Z-axis",
-      imageLink: "https://en.wikipedia.org/wiki/Thrasher_Presents_Skate_and_Destroy#/media/File:Thrasher_-_Skate_and_Destroy_Coverart.jpg",
+      imageLink: "https://upload.wikimedia.org/wikipedia/en/1/18/Thrasher_-_Skate_and_Destroy_Coverart.jpg",
       type: "Sports",
       hashtags: ["#thrasherpresentsskateanddestroy", "#playstation", "realSport"]
     };
@@ -67,22 +67,23 @@ export class AppComponent {
       title: "Max Payne 3",
       body: "Max Payne 3 is a third-person shooter video game developed by Rockstar Studios and published by Rockstar Games. It was released on May 15, 2012 for the PlayStation 3 and Xbox 360; a Microsoft Windows port was released on May 29, 2012, followed by an OS X port on June 20, 2013.",
       author: "Paul MacPherson",
-      imageLink: "https://en.wikipedia.org/wiki/Max_Payne_3#/media/File:Max_Payne_3_Cover.jpg",
+      imageLink: "https://upload.wikimedia.org/wikipedia/en/2/21/Max_Payne_3_Cover.jpg",
       type: "Third-person shooter",
       hashtags: ["#max_payne_3", "#shooting", "#third-person_shooter", "#gun"]
     };
 
-    this.ListOfContent = new List(this.ContentItem1);
-    this.ListOfContent = new List(this.ContentItem2);
-    this.ListOfContent = new List(this.ContentItem3);
-    this.ListOfContent = new List(this.ContentItem4);
-    this.ListOfContent = new List(this.ContentItem5);
+    this.ListOfContent.addContent(this.ContentItem1);
+    this.ListOfContent.addContent(this.ContentItem2);
+    this.ListOfContent.addContent(this.ContentItem3);
+    this.ListOfContent.addContent(this.ContentItem4);
+    this.ListOfContent.addContent(this.ContentItem5);
 
 
-    console.log("View the array of items in the list: ", this.ListOfContent);
-    console.log("View the array of items in the list: ", this.ContentItem2);
+    // console.log("View the array of items in the list: ", this.ListOfContent);
+    // console.log("View the array of items in the list: ", this.ContentItem2);
 
-
+    // console.log(this.ListOfContent.displayArray);
+    // console.log(this.ListOfContent.addContent);
   }
 
 
--- Assignment1/B_Patel_RockstarGames/src/app/models/list.ts
+++ Assignment1/B_Patel_RockstarGames/src/app/models/list.ts
@@ -1,18 +1,31 @@
 
 import { Content } from "./content";
 
+
 export class List {
-  static contentCount = 0;
+
   private _items: Content[];
-  constructor(item: Content) {
+
+  constructor() {
     this._items = []; // initialize array
-    this._items[0] = item; // set the value of the item at the first index of the array
   }
+
+
   get items(): Content[] {
     return this._items;
   }
+
+  addContent(Content: any) {
+    this._items.push(Content);
+  }
+
+  lengthOfArray() {
+    return this._items.length;
+  }
+
   // addContent() {
-  //   return this.items.push;
+  //   let input = this.items;
+  //   return this.items.push(input);
   // }
   // lengthOfContent(): number {
   //   return this.items.length;
@@ -23,8 +36,32 @@ export class List {
   //     return console.log(this.lengthOfContent);
   //   }
   // }
-  increaseCount() {
-    return List.contentCount++;
+
+
+  displayContent(index: number) {
+    for (var i = 0; i < this.lengthOfArray(); i++) {
+      if (this._items[i].id === index) {
+        return `
+       <div class="card border border-2 mb-3 border-success" style="width: 18rem;">
+       <img src="${this._items[index].imageLink}" class="card-img-top" alt="...">
+         <div class="card-body">
+           <h1 class="text-dark text-center card-title">Title : <strong>${this._items[index].title}</strong></h1>
+           <h3 class="mb-3 text-dark card-text">Type : ${this._items[index].type}</h3>
+           <h3 class="mb-3 text-dark card-text">Author : ${this._items[index].author}</h3>
+           <p class="mb-3 text-dark card-text">Body : ${this._items[index].body}</p>
+           <p class="mb-3 text-primary card-text">HashTags : ${this._items[index].hashtags?.join(' ')}</p>
+         </div>
+     </div>
+              `
+      }
+    }
+    return this.errorHtml(index);
+
+
+  }
+
+  errorHtml(index: number) {
+    return `<div class="text-center alert alert-danger">There is no data stored at Index: <strong>${index} </strong>in the array</div>`
   }
 
 
