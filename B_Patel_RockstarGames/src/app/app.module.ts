feb8c3321e32c8b2231071f6dd02494e09c4e206
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-05-22 00:37:36 -0400

    ContentCard and ContentList component added

--- B_Patel_RockstarGames/src/app/app.module.ts
+++ B_Patel_RockstarGames/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentListComponent,
+    ContentCardComponent
   ],
   imports: [
     BrowserModule
