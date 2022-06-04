1e110aa3c6f6e85890c67611dd98149a54817853
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-06-04 15:40:23 -0400

    Binding done

--- B_Patel_RockstarGames/src/app/directives/highlight-important-data.directive.ts
+++ B_Patel_RockstarGames/src/app/directives/highlight-important-data.directive.ts
@@ -5,33 +5,59 @@ import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angula
 })
 export class HighlightImportantDataDirective {
   @Input() color?: string;
-  private isHighlighted: boolean = false;
+  private isHighLightBorder: boolean = false;
+  private isHighLightBgColor: boolean = false;
+  private isHightLightTextColor: boolean = false;
+
   private initialTypeBorder: string;
   private initialTitleBgColor: string;
-  // private initialTextColor: string;
+  private initialTextColor: string;
+
   @HostBinding('style.border')
   get borderForType() {
-    return this.isHighlighted ? '5px double red' : this.initialTypeBorder;
+    return this.isHighLightBorder ? '5px double red' : this.initialTypeBorder;
   }
   @HostBinding('style.backgroundColor')
   get backgroundColorForTitle() {
-    return this.isHighlighted ? this.color || "transparent" :
+    return this.isHighLightBgColor ? this.color || "transparent" :
       this.initialTitleBgColor;
   }
+
+  @HostBinding('style.color')
+  get textColorForTags() {
+    return this.isHightLightTextColor ? 'red' : this.initialTextColor;
+  }
+
+
+
   constructor(private el: ElementRef) {
     this.initialTypeBorder = this.el.nativeElement.style.border;
     this.initialTitleBgColor = this.el.nativeElement.style.backgroundColor;
+    this.initialTextColor = this.el.nativeElement.style.color;
   }
 
   @HostListener('mouseenter') onclickTitle() {
-    this.isHighlighted = !this.isHighlighted;
+    this.isHighLightBorder = !this.isHighLightBorder;
+    this.isHightLightTextColor = !this.isHightLightTextColor;
+
   }
+
   @HostListener('mouseout') onClickTitle() {
-    this.isHighlighted = !this.isHighlighted;
+    this.isHighLightBorder = !this.isHighLightBorder;
+    this.isHightLightTextColor = !this.isHightLightTextColor;
+
     // console.log(this.color);
   }
+  // @HostListener('mouseenter') onOverTag() {
+  //   this.isHightLightTextColor = !this.isHightLightTextColor;
+  // }
+  // @HostListener('mouseout') onOutTag() {
+  //   this.isHightLightTextColor = !this.isHightLightTextColor;
+
+  //   // console.log(this.color);
+  // }
   @HostListener('click') onClickType() {
-    this.isHighlighted = !this.isHighlighted;
+    this.isHighLightBgColor = !this.isHighLightBgColor;
     // console.log(this.color);
   }
 
