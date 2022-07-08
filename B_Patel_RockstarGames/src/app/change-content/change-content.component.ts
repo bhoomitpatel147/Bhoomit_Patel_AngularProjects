e76a49dbc54ee776f6ca9b749ba458f93e1be17e
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-07-08 15:49:14 -0400

    CSS added for form

--- B_Patel_RockstarGames/src/app/change-content/change-content.component.ts
+++ B_Patel_RockstarGames/src/app/change-content/change-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '../models/content';
+import { RockStarGamesService } from '../services/rock-star-games.service';
 
 @Component({
   selector: 'app-change-content',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-content.component.scss']
 })
 export class ChangeContentComponent implements OnInit {
+  contentItem: Content = {
+    title: "",
+    body: "",
+    author: "",
+  };
+  tempTags: string = '';
 
-  constructor() { }
+  constructor(private contentService: RockStarGamesService) { }
+
 
   ngOnInit(): void {
+  }
+  addContentToServer(): void {
+    this.contentItem.hashtags = this.tempTags.split(", ");
+    this.contentService.addContentItem(this.contentItem)
+      .subscribe(newContentFromServer =>
+        console.log("Success! New content added", newContentFromServer)
+      );
+  }
+  updateContentOnServer(): void {
+    this.contentItem.hashtags = this.tempTags.split(", ");
+    this.contentService.updateContent(this.contentItem)
+      .subscribe(() =>
+        console.log("Content updated successfully", this.contentItem)
+      );
   }
 
 }
