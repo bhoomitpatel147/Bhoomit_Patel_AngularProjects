454843ac7b4f33741d8fac64aff04021f7168f30
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-07-10 21:11:41 -0400

    Button and h1 text will change according to the condition

--- B_Patel_RockstarGames/src/app/change-content/change-content.component.ts
+++ B_Patel_RockstarGames/src/app/change-content/change-content.component.ts
@@ -32,27 +32,27 @@ export class ChangeContentComponent implements OnInit {
 
 
   ngOnInit() {
-
     this.route.paramMap.subscribe(params => {
 
-      this.id = +(params.get('id') ?? 0); // uses the + unary operator
+      this.id = +(params.get('id') ?? -1); // uses the + unary operator
+      if (this.id != -1) {
+        this.contentService.getContentItem(this.id).subscribe(singleItem => {
 
+          if (singleItem) {
+            // this.updateItem = singleItem;
 
-      this.contentService.getContentItem(this.id).subscribe(singleItem => {
-
-        if (singleItem) {
-          // this.updateItem = singleItem;
-          this.contentItem.id = this.id;
-          console.log("new id" + this.id);
-        }
-      },
-        error => {
-          return this.router.navigateByUrl('contentNotFound');
-        }
-      );
+            this.contentItem.id = this.id;
+            console.log("new id" + this.id);
+          }
+        },
+          error => {
+            return this.router.navigateByUrl('contentNotFound');
+          }
+        );
+      }
     });
-  }
 
+  }
 
   addContentToServer(): void {
     this.contentItem.hashtags = this.tempTags.split(", ");
